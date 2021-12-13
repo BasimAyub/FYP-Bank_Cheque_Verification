1d5fc0b9f6eaba67ec478b363cb3b9663f4f17fa
Rehan Ashraf <m.rd7447@gmail.com>
2021-12-13 07:23:53 +0500

    fix complain page ui

--- front-end/src/screens/user/complain/registerComplain.js
+++ front-end/src/screens/user/complain/registerComplain.js
@@ -1,34 +1,46 @@
-import React from 'react'
-import UserTemplate from '../userTemplate'
+import React from "react";
+import UserTemplate from "../userTemplate";
+import "./registerComplain.css"
+
 
 function RegisterComplain() {
-    return (
-        <UserTemplate>
-            <div>
-            <form>
-  <div class="form-group">
-    <label for="exampleFormControlInput1">Email address</label>
-    <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com"/>
-  </div>
-  <div class="form-group">
-    <label for="exampleFormControlSelect1">Example select</label>
-    <select class="form-control" id="exampleFormControlSelect1">
-      <option>1</option>
-      <option>2</option>
-      <option>3</option>
-      <option>4</option>
-      <option>5</option>
-    </select>
-  </div>
-  
-  <div class="form-group">
-    <label for="exampleFormControlTextarea1">Example textarea</label>
-    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-  </div>
-</form>
-            </div>
-        </UserTemplate>
-    )
+  return (
+    <UserTemplate>
+      <div className="mainDivRC">
+        <h1>Register Complain</h1>
+        <form>
+          <div class="form-group">
+            <label for="exampleFormControlInput1">Email address</label>
+            <input
+              type="email"
+              class="form-control"
+              id="exampleFormControlInput1"
+              placeholder="name@example.com"
+            />
+          </div>
+          <div class="form-group">
+            <label for="exampleFormControlSelect1">Example select</label>
+            <select class="form-control" id="exampleFormControlSelect1">
+              <option>1</option>
+              <option>2</option>
+              <option>3</option>
+              <option>4</option>
+              <option>5</option>
+            </select>
+          </div>
+
+          <div class="form-group">
+            <label for="exampleFormControlTextarea1">Example textarea</label>
+            <textarea
+              class="form-control"
+              id="exampleFormControlTextarea1"
+              rows="3"
+            ></textarea>
+          </div>
+        </form>
+      </div>
+    </UserTemplate>
+  );
 }
 
-export default RegisterComplain
+export default RegisterComplain;
